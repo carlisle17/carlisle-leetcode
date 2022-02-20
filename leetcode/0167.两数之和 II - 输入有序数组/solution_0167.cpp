2fa1dc0fe595bd9a168bc3a84168d1116a27235e
Carlisle Cullen <1523409045@qq.com>
2022-02-21 00:08:53 +0800

    Create solution_0167.cpp

---
+++ leetcode/0167.两数之和 II - 输入有序数组/solution_0167.cpp
@@ -0,0 +1,13 @@
+// 双指针：时间复杂度为O(n)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0, right = numbers.size()-1;
+        while(left < right){
+            if(numbers[left] + numbers[right] == target) return {left + 1,right + 1};
+            else if(numbers[left] + numbers[right] < target) ++left;
+            else --right;
+        }
+        return {};
+    }
+};
