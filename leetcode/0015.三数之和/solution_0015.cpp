578904dc0217010f88f80cd97f167c95b6592f69
Carlisle Cullen <1523409045@qq.com>
2022-02-20 23:54:30 +0800

    Update solution_0015.cpp

--- leetcode/0015.三数之和/solution_0015.cpp
+++ leetcode/0015.三数之和/solution_0015.cpp
@@ -10,7 +10,8 @@ public:
         for (int i = 0; i < nums.size(); ++i) {
             for (int j = i + 1; j < nums.size(); ++j) {
                 for (int k = j + 1; k < nums.size(); ++k) {
-                    if (nums[i] + nums[j] + nums[k] == 0) ans.push_back({ nums[i], nums[j], nums[k] });
+                    if (nums[i] + nums[j] + nums[k] == 0)
+                        ans.push_back({ nums[i], nums[j], nums[k] });
                 }
             }
         }
@@ -26,4 +27,35 @@ public:
     }
 };
 
-// 排序+双指针：
+// 排序+双指针：时间复杂度为O(n^2)，排序的时间复杂度小于两层循环，故取大的时间复杂度
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        // 考虑特殊情况
+        int length = nums.size();
+        if(length < 3) return {};
+
+        // 设置返回的结果数组
+        vector<vector<int>> result;
+        
+        // 对原数组进行排序，使得每个子数组都是以由小至大的顺序压入的
+        sort(nums.begin(), nums.end());
+        // 进行第一层循环，确定第一位数字
+        for(int index = 0; index < length - 2; ++index){
+            // 剪枝：如果第一位数字与上次的数字相同，则直接跳过
+            if(0 < index && nums[index] == nums[index-1])
+                continue;
+            // 双指针：根据和目标值的大小，调整指针的移动，需要遍历整个子数组，时间复杂度为O(n)
+            int left = index + 1, right = length - 1;
+            while(left < right){
+                int sum = nums[left] + nums[right];
+                if(sum + nums[index] == 0) result.push_back({nums[index],nums[left],nums[right]});
+                if(sum + nums[index] < 0) ++left;
+                else --right;
+            }
+        }
+        // 由于第一位数字不会重复，只会后两个数字指针在运动，所以每个重复的子数组会跟在其数组之后，不需要再次进行排序（已完成排序）
+        result.erase(unique(result.begin(),result.end()),result.end());
+        return result;
+    }
+};
